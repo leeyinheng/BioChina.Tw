f5cb76377014ffeba12de4a56a17af5e814771b5
Henry Lee <leeyinheng@yahoo.com>
2021-08-25 15:39:03 +0800

    beyongwood update

--- beyondwood/food-shop/js/script.js
+++ beyondwood/food-shop/js/script.js
@@ -496,3 +496,30 @@ jQuery(function ($) {
         document.getElementById('fade').style.display = 'none';
         lightBoxVideo.pause();
       }
+
+
+        /* ===================================
+             Customer Q&A Script 
+    ======================================*/
+
+      $(function(){
+        
+        function setHeight(){
+            $(".response").each(function(index,element){
+                var target = $(element);
+                target.removeClass("fixed-height");
+                var height = target.innerHeight();
+                target.attr("data-height", height)
+                      .addClass("fixed-height");
+            });
+        };
+        
+        $("input[name=question]").on("change", function(){
+            $("p.response").removeAttr("style");
+            
+            var target = $(this).next().next();
+            target.height(target.attr("data-height"));
+        })
+        
+        setHeight();
+    });
